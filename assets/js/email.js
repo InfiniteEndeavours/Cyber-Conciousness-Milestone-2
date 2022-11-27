d1408306aaba3263040024ee6071a48d4d26556b
InfiniteEndeavours <ethan.pitt@protonmail.com>
2022-11-27 21:17:00 +0000

    Rework email.js - Remove onsubmit from form element

--- assets/js/email.js
+++ assets/js/email.js
@@ -1,15 +1,8 @@
-function sendMail (contactForm) {
-    emailjs.send("gazeEmail","template_7ks6kxp", {
-        "from_name": contactForm.fullName.value,
-        "from_email": contactForm.emailAddress.value,
-        "message": contactForm.submission.value
-    })
-        .then(
-            function (response) {
-                console.log("Success", response);
-            },
-            function (error) {
-                console.log("Error", error);
-            }
-        );
-}
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    emailjs.sendForm('gazeEmail', 'template_7ks6kxp', this)
+        .then(function() {
+            console.log('SUCCESS!');
+        }, function(error) {
+            console.log('FAILED...', error);
+        });
+});
